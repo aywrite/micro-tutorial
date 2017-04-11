ec74a61fcaaf07c9fdb5297f49764d9cf519f450
Andrew Wright <andrewwright@andrew-wright.dhcp.syd2.internal>
2017-04-11 16:43:09 +1000

    Add zipkin tracing to app

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 import os
+import requests
 import psycopg2
 from flask import (
     Flask,
@@ -12,6 +13,7 @@ from flask import (
     render_template,
     flash
 )
+from py_zipkin.zipkin import zipkin_span
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -24,11 +26,40 @@ app.config.update(dict(
     DBPASSWORD=os.getenv('PG_PWORD', ''),
     SECRET_KEY='DN2DaxN9LNmL85VS',
     USERNAME='admin',
-    PASSWORD='default'
+    PASSWORD='default',
+    ZP_HOST=os.getenv('ZP_HOST', 'localhost')
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 Entry = namedtuple('entry', ['title', 'text'])
+
+
+def http_transport(encoded_span):
+    # The collector expects a thrift-encoded list of spans. Instead of
+    # decoding and re-encoding the already thrift-encoded message, we can just
+    # add header bytes that specify that what follows is a list of length 1.
+    host = ''.join(['http://', app.config['ZP_HOST'], r':9411/api/v1/spans'])
+    body = '\x0c\x00\x00\x00\x01' + encoded_span
+    requests.post(
+        host,
+        data=body,
+        headers={'Content-Type': 'application/x-thrift'},
+    )
+
+
+def init_db():
+    db = get_db()
+    cur = db.cursor()
+    cur.execute(
+        """CREATE TABLE entries (
+        id serial PRIMARY KEY,
+        title text NOT NULL,
+        text text NOT NULL);
+        """
+    )
+    db.commit()
+    cur.close()
+
 
 def connect_db():
     """Connects to the specific database."""
@@ -60,28 +91,54 @@ def close_db(error):
 
 @app.route('/')
 def show_entries():
+    with zipkin_span(
+        service_name='default',
+        span_name='entries_endpoint',
+        transport_handler=http_transport,
+        sample_rate=100, # Value between 0.0 and 100.0
+    ):
+        entries = get_entries()
+        return render_template('show_entries.html', entries=entries)
+
+
+@zipkin_span(service_name='default', span_name='get_entries')
+def get_entries(limit=10):
     db = get_db()
     cur = db.cursor()
-    cur.execute('select title, text from entries order by id desc')
+    cur.execute('SELECT title, text FROM entries ORDER BY id DESC LIMIT (%s)',
+                ([limit]))
     entries = cur.fetchall()
     entries = [Entry(*entry) for entry in entries]
-    print entries
     cur.close()
-    return render_template('show_entries.html', entries=entries)
+    return entries
+
+
+def format_entries(entries):
+    formated_entries = []
+    for entry in entries:
+        new_entry = Entry(entry.title.title(), entry.text)
+        formated_entries.append(new_entry)
+    return new_entry
 
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    cur = db.cursor()
-    cur.execute('insert into entries (title, text) values (%s, %s)',
-                 ([request.form['title'], request.form['text']]))
-    db.commit()
-    cur.close()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+    with zipkin_span(
+        service_name='default',
+        span_name='add_entry_endpoint',
+        transport_handler=http_transport,
+        sample_rate=100
+    ):
+        if not session.get('logged_in'):
+            abort(401)
+        db = get_db()
+        cur = db.cursor()
+        cur.execute('insert into entries (title, text) values (%s, %s)',
+                     ([request.form['title'], request.form['text']]))
+        db.commit()
+        cur.close()
+        flash('New entry was successfully posted')
+        return redirect(url_for('show_entries'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
